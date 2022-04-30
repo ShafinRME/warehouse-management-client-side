a9e15d7b7d4964fbb25b748a498176532915f9f2
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-30 16:55:18 +0600

    Fakedata is prepared

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
-        <Route path='/' element={ }></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+        {/* <Route path='/' element={ }></Route> */}
       </Routes>
+      <Footer></Footer>
 
     </div>
   );
