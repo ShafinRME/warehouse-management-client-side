41fd77868237d4efd52ee9e058b99c33c3951986
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-01 11:36:57 +0600

    Footer is done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='container'>
+            <p>Hi i am banner</p>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,8 +3,19 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer>
-            <p>Spacex-car-inventory</p>
+        <footer className='container footer-container'>
+            <div className='text-center'>
+                <h4>SpaceX Car House</h4>
+                <p className='footer-description'>SpaceX Car House is an Car Inventory.Here you can get your best car in recent world.If You want cope-up  with this modern car world then you must to stay tuned with us , Just follow us bellow ... </p>
+                <div>
+                    <a href=""> <img src="https://i.ibb.co/0Xf7XpS/google.png" alt="" /></a>
+                    <a href=""> <img src="https://i.ibb.co/PrcScz9/facebook.png" alt="" /></a>
+                    <a className='p-4' href=""><img src="https://i.ibb.co/djNwppJ/github.png" alt="" /></a>
+                </div>
+            </div>
+            <hr />
+
+            <p className='d-flex last-line'> Copyright &copy; {new Date().getFullYear()} <div className='horizontal-line'>  </div> Spacex-car-inventory</p>
         </footer>
     );
 };
