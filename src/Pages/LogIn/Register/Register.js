f5aef08ff92b3df89f62e3e91f1d85a7a8364f3f
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-05 15:31:36 +0600

    Login and Register Form is completed toggle is also done

--- src/Pages/LogIn/Register/Register.js
+++ src/Pages/LogIn/Register/Register.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Register.css'
 
 const Register = () => {
+    const handleRegister = event => {
+        event.preventDefault();
+    }
     return (
-        <div>
+        <div className='register-form' >
             <h2>Please Register</h2>
-            <form>
-                <input type="text" name="" id="" />
+            <form onClick={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email' required />
+                <input type="password" name="password" id="" placeholder='Your Password' required />
+                <input type="submit" value="Register" />
             </form>
-
+            <p>Already Have an Account ? <Link to="/login" className='text-info text-decoration-none' >Please Login Here</Link> </p>
         </div>
     );
 };
